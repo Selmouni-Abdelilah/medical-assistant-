6e8979150b043dbdf0dfd0d20a8e27d7e5fd8e09
Mouad Ait Hammou <105555280+feevemouad@users.noreply.github.com>
2024-12-31 11:20:28 +0100

    Update settings ( get env variables connection strings ).py

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ def get_env_variable(key):
     """
     Get an environment variable or raise an exception.
     """
-    value = os.environ.get(f'ConnectionStrings:{key}')
+    value = os.environ.get(f'CUSTOMCONNSTR_:{key}')
     if not value:
         raise ValueError(f"Environment variable {key} is not set.")
     return value
